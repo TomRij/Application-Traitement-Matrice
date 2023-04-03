d6650bf1f2d1fc2d22600ab9e700198d4a842ae5
Tom Rijckaert <200082@site.asty-moulin.be>
2023-04-03 23:21:27 +0200

    commencement du code c# des applications tris

---
+++ 5TTI_ApplicationTraitementMatrice_RijckaertTom/5TTI_ApplicationTraitementMatrice_RijckaertTom/MethodeDuProjet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tom_Rijckaert_Code_Tris
+{
+    public struct MethodeDuProjet
+    {
+
+        Generation_Tableau(int nbLigne, int nbColonne, out int[,] tabAlea)
+        {
+            for (int ligne = 0; ligne < nbLigne-1; ligne++)
+            {
+                for (int colonne = 0; colonne < nbColonne-1; colonne++)
+                {
+                    tabAlea[ligne, colonne] = tabAlea.next(0, 20)
+                }
+            }
+
+
+        }
+
+
+    }
+}
